6c480c868d5d8b0434e59107263d2a5d49605a80
Romisaa <romisaa.hamdy@yahoo.com>
2024-01-22 13:36:40 -0800

    edit

--- sort.h
+++ sort.h
@@ -29,7 +29,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, size_t low, size_t high, size_t size);
+size_t lomuto_partition(int *array, size_t low, size_t high, size_t size);
 void quick_sort_rec(int *array, int low, int high, size_t size);
 
 #endif
